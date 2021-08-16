8fc0d7f8981ac09f8a998ada2cc3419cb705e697
stoneykilla <austinscottbeatz@gmail.com>
2021-08-16 10:16:46 +0500

    init

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ChakraProvider } from "@chakra-ui/react"
+import Navbar from "../components/Navbar";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Navbar/>
+    </ChakraProvider>
+  );
+}
+
+export default App;
