5443b889bbb33ecc529c9019444b5189302d44c6
Kirill Kumma <kirill.kumma@gmail.com>
2021-08-21 18:37:57 +0500

    server-api: add methods to Category controller

--- server-api/app/controllers/category_controller.rb
+++ server-api/app/controllers/category_controller.rb
@@ -1,2 +1,11 @@
 class CategoryController < ApplicationController
+  def index
+    @categories = Category.all
+    json_response(@categories)
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    json_response(@category)
+  end
 end
