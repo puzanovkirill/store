f18daaa8827f4e888665a928a013ac93dad9032f
Kirill Kumma <kirill.kumma@gmail.com>
2021-08-21 22:00:26 +0500

    server-api: add db seeding

--- server-api/app/models/category.rb
+++ server-api/app/models/category.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  has_many :products
+  has_many :products, dependent: :destroy
   validates_presence_of :name
 end
--- server-api/db/seeds.rb
+++ server-api/db/seeds.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+Product.destroy_all
+Property.destroy_all
+
+phones = Category.create(name: 'Phones')
+laptops = Category.create(name: 'Laptops')
+
+i_phone = Product.create(name: 'iPhone 12', price: 699, category_id: phones.id)
+pixel = Product.create(name: 'Google Pixel 4', price: 449, category_id: phones.id)
+macbook = Product.create(name: 'MacBook Pro 13 M1', price: 1299, category_id: laptops.id)
+matebook = Product.create(name: 'Huawei Matebook 13', price: 999, category_id: laptops.id)
+
+Property.create(name: 'Color', value: 'Blue', product_id: i_phone.id)
+Property.create(name: 'Color', value: 'Black', product_id: pixel.id)
+Property.create(name: 'Operating System', value: 'iOS', product_id: i_phone.id)
+Property.create(name: 'Operating System', value: 'Android', product_id: pixel.id)
+Property.create(name: 'Brand', value: 'Apple', product_id: i_phone.id)
+Property.create(name: 'Brand', value: 'Google', product_id: pixel.id)
+Property.create(name: 'Color', value: 'Silver', product_id: macbook.id)
+Property.create(name: 'Color', value: 'Silver', product_id: matebook.id)
+Property.create(name: 'Brand', value: 'Apple', product_id: macbook.id)
+Property.create(name: 'Brand', value: 'Huawei', product_id: matebook.id)
