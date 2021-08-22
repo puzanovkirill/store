51304ab49e044abcc10262baec1688ea4e78b25f
Kirill Kumma <kirill.kumma@gmail.com>
2021-08-22 17:40:51 +0500

    server-api: calculate total price of order in Cart

--- server-api/app/controllers/cart_controller.rb
+++ server-api/app/controllers/cart_controller.rb
@@ -2,23 +2,26 @@ class CartController < ApplicationController
   before_action :authorize_request
 
   def index
-    json_response obj: { products: cart_products }
+    json_response obj: cart_res
   end
 
   def add_item
     product = Product.find(params[:id])
     CartItem.create(cart_id: current_user.cart.id, product_id: product.id)
-    json_response obj: { products: cart_products }
+    json_response obj: cart_res
   end
 
   def remove_item
     CartItem.find_by(product_id: params[:id], cart_id: current_user.cart.id).destroy
-    json_response obj: { products: cart_products }
+    json_response obj: cart_res
   end
 
   private
 
-  def cart_products
-    current_user.cart.cart_items.map { |item| item.product_id }  
+  def cart_res
+    {
+      products: current_user.cart.cart_items.map { |item| item.product_id },
+      total: current_user.cart.cart_items.reduce(0) { |sum, item| sum + item.product.price }
+    }
   end
 end
