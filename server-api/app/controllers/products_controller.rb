8406f6e09b01dc514dbf92f7a51a33f68e81f428
Kirill Kumma <kirill.kumma@gmail.com>
2021-08-26 12:33:22 +0500

    server-api: filter products by brand and category

--- server-api/app/controllers/products_controller.rb
+++ server-api/app/controllers/products_controller.rb
@@ -2,7 +2,16 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:categories] and params[:brands]
+      @products = Product.where(category_id: params[:categories]).where(brand_id: params[:brands])
+    elsif params[:categories]
+      @products = Product.where(category_id: params[:categories])
+    elsif params[:brands]
+      @products = Product.where(brand_id: params[:brands])
+    else
+      @products = Product.all 
+    end
+
     json_response obj: @products
   end
 
