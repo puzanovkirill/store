db23c4f3970455425ccb5079dffc049a61c42e93
Kirill Kumma <kirill.kumma@gmail.com>
2021-08-26 13:44:27 +0500

    server-api: refactor Products controller

--- server-api/app/controllers/products_controller.rb
+++ server-api/app/controllers/products_controller.rb
@@ -2,16 +2,25 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = if params[:categories] && params[:brands]
-                  Product.where(category_id: params[:categories]).where(brand_id: params[:brands])
-                elsif params[:categories]
-                  Product.where(category_id: params[:categories])
-                elsif params[:brands]
-                  Product.where(brand_id: params[:brands])
-                else
-                  Product.all
-                end
+  #  @products = if params[:categories] && params[:brands]
+              #    Product.where(category_id: params[:categories]).where(brand_id: params[:brands])
+              #  elsif params[:categories]
+              #    Product.where(category_id: params[:categories])
+              #  elsif params[:brands]
+              #    Product.where(brand_id: params[:brands])
+              #  else
+              #    Product.all
+              #   end
+    
+    @products = Product.all
 
+    if params[:categories]
+      @products = @products.where(category_id: params[:categories])
+    end
+
+    if params[:brands]
+      @products = @products.where(brand_id: params[:brands])
+    end
     json_response obj: @products
   end
 
