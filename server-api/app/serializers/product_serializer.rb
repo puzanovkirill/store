4abb47bcfa58f9c5f8e1dc8d1fe97909d6e4d644
Kirill Kumma <kirill.kumma@gmail.com>
2021-08-26 15:55:51 +0500

    server-api: add image field to product serializer

--- server-api/app/serializers/product_serializer.rb
+++ server-api/app/serializers/product_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :category_id, :brand_id
+  attributes :id, :name, :price, :category_id, :brand_id, :image
   has_many :properties
 end
